de8d88ffc94c3af1087ca5902a51115a67a6423f
Dhanraj Gatti <140165143+DhanrajDevOps@users.noreply.github.com>
2024-04-18 12:40:20 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-sudo docker pull dhanrajgatti13docker/simple-python-flask-app
+sudo docker pull dhanrajgatti13docker/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-sudo docker run -d -p 5000:5000 dhanrajgatti13docker/simple-python-flask-app
+sudo docker run -d -p 5000:5000 dhanrajgatti13docker/simple-python-flask-app:latest
