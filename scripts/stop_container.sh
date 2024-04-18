9905513203746be2299718226f1cc43e52c104fc
Dhanraj Gatti <140165143+DhanrajDevOps@users.noreply.github.com>
2024-04-18 12:08:45 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+sudo chmod +x /path/to/scripts/stop_container.sh
+
 # stop the running container(if any)
 containerid = `sudo docker ps | awk -F " " '{print $1}'`
 sudo docker rm -f $containerid
